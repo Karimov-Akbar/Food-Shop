c8ebc6b4ae209c1e844f072740b4b74c2760009e
Karimov-Akbar <akbarkarimov250@gmail.com>
2026-02-18 21:20:38 +0500

    fixes

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,5 @@
 import Sidebar from "../widgets/sidebar/ui/Sidebar";
-import Menu from "../pages/menu/ui/Menu";
 import './styles/index.css'
-import ProductInfo from "../pages/info/ui/ProductInfo";
 import AppRouter from "./providers/routers/AppRouter";
 
 const App = () => {
