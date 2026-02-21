0cfe6a4a74e386f9eae6c95dde3a99934870c13e
Karimov-Akbar <akbarkarimov250@gmail.com>
2026-02-21 12:04:12 +0500

    add search logic

--- src/pages/menu/ui/Menu.jsx
+++ src/pages/menu/ui/Menu.jsx
@@ -3,12 +3,13 @@ import search from '../../../shared/icons/search.svg'
 import '../../../app/styles/index.css'
 import './Menu.css'
 import ProductCard from '../../../widgets/productcard/ui/ProductCard'
-import { productsApi } from '../../../shared/api/products'
+import { productsApi, searchProducts } from '../../../shared/api/products'
 import { useEffect, useState } from 'react'
 import Paginaton from '../../../shared/ui/pagination/Pagination'
 import { useSearchParams } from 'react-router-dom'
 
 const Menu = () => {
+    const [searchQuery, setSearchQuery] = useState(''); 
     const [searchParams, setSearchParams] = useSearchParams();
     const [products, setProducts] = useState([]);
     const [totalPages, setTotalPages] = useState(0);
@@ -17,19 +18,27 @@ const Menu = () => {
     
     const fetchProducts = (page) => {
         const skip = (page - 1) * LIMIT;
-        productsApi(LIMIT, skip).then(data => { 
+        const request = searchQuery.trim() ? searchProducts(searchQuery, LIMIT, skip) : productsApi(LIMIT, skip);
+        request.then(data => { 
             setProducts(data.products);
             setTotalPages(Math.ceil(data.total / LIMIT));
             window.scrollTo(0,0);
         });
     }
 
+    useEffect(()=> {
+        const timer = setTimeout(()=>{
+            setSearchParams({page: 1});
+        }, 500);
+        return () => clearTimeout(timer);
+    }, [searchQuery]);
+
 
     useEffect(() => {
         fetchProducts(currentPage);
-    }, [currentPage]);
+    }, [currentPage, searchQuery]);
 
-    const handlePageChange = (page) =>{
+    const handlePageChange = (page) => {
         setSearchParams({ page: page + 1});
     }
 
@@ -45,6 +54,8 @@ const Menu = () => {
                         <img src={search} alt="Search" />
                         <input type="text" 
                             placeholder='Введите название товара'
+                            value={searchQuery}
+                            onChange={(e) => setSearchQuery(e.target.value)}
                         />
                     </div>
                 </div>
--- src/shared/api/products.js
+++ src/shared/api/products.js
@@ -7,3 +7,8 @@ export const productId = async (id) => {
     const response = await fetch(`https://dummyjson.com/products/${id}`);
     return response.json();
 };
+
+export const searchProducts = async (query, limit = 30, skip = 0) =>{
+    const response = await fetch(`https://dummyjson.com/products/search?q=${query}&limit=${limit}&skip=${skip}`);
+    return response.json();
+}
